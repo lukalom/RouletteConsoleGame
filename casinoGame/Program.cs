0f811ccfc2fe50acff39665d06c01fc6e4815217
Luka Lomiashvili <74249289+lukalom@users.noreply.github.com>
2021-12-23 23:28:33 +0400

    Fix Winning Logic

--- casinoGame/Program.cs
+++ casinoGame/Program.cs
@@ -1,15 +1,11 @@
 ﻿double balance = 1000;
 int[] red = { 32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3 };
-int[] black = { 15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26 };
+
 
 while (balance != 0)
 {
     Random r = new Random();
-    int RandomNumber = r.Next(0, 36);
-    int RandomColor = r.Next(1, 3);
-    int ComputerRandomNumberRed = red[r.Next(0, 18)];
-    int ComputerRandomNumberBlack = black[r.Next(0, 18)];
-    int ComputerRandomNumberColor = (RandomColor == 1) ? ComputerRandomNumberRed : ComputerRandomNumberBlack;
+    int RandomNumber = r.Next(0, 37);
 
     Console.Write("(1) only numbers, (2) only colors (3) both should match: ");
     bool isGameMode = int.TryParse(Console.ReadLine(), out int gameMode);
@@ -42,12 +38,12 @@ while (balance != 0)
         if (gameMode == 1)
         {
             Console.Write("Enter Number Between (0, 35) range: ");
-            bool isBetingNumber = int.TryParse(Console.ReadLine(), out int roulletNumber);
+            bool isBetingNumber = int.TryParse(Console.ReadLine(), out int rouletteNumber);
 
-            if (isBetingNumber && (roulletNumber >= 0 && roulletNumber < 36))
+            if (isBetingNumber && (rouletteNumber >= 0 && rouletteNumber < 36))
             {
 
-                if (RandomNumber == roulletNumber)
+                if (RandomNumber == rouletteNumber)
                 {
                     balance += betMoney * 2;
                     Console.WriteLine($"you won: {betMoney * 2}: current balance {balance}");
@@ -69,20 +65,25 @@ while (balance != 0)
         if (gameMode == 2)
         {
             Console.Write("choose color number 1) black  2) red : ");
-            bool isBetingColor = int.TryParse(Console.ReadLine(), out int roulletColor);
+            bool isBetingColor = int.TryParse(Console.ReadLine(), out int rouletteColor);
 
-            if (isBetingColor && (roulletColor == 1 || roulletColor == 2))
+            if (isBetingColor && (rouletteColor == 1 || rouletteColor == 2))
             {
 
-                if (RandomColor == roulletColor)
+                if (red.Contains(RandomNumber) && rouletteColor == 2)
                 {
                     balance += betMoney + (betMoney / 5);
-                    Console.WriteLine($"you won: {betMoney + (betMoney / 5)}: current balance {balance}: computer color {RandomColor}");
+                    Console.WriteLine($"you won: {betMoney + (betMoney / 5)}: current balance {balance}");
+                }
+                else if (!red.Contains(RandomNumber) && rouletteColor == 1)
+                {
+                    balance += betMoney + (betMoney / 5);
+                    Console.WriteLine($"you won: {betMoney + (betMoney / 5)}: current balance {balance}");
                 }
                 else
                 {
                     balance -= betMoney;
-                    Console.WriteLine($"you lost: {betMoney} current balance {balance} collor number {RandomColor}");
+                    Console.WriteLine($"you lost: {betMoney} current balance {balance}");
 
                 };
 
@@ -97,37 +98,64 @@ while (balance != 0)
 
         }
 
-        //colors and numbers should match exaclty win 5X
+        //colors and numbers should match exaclty win 2X + 20%
         if (gameMode == 3)
         {
 
             Console.Write("choose color number 1) black  2) red : ");
-            bool isBetingColor = int.TryParse(Console.ReadLine(), out int roulletColor);
+            bool isBetingColor = int.TryParse(Console.ReadLine(), out int rouletteColor);
 
             Console.Write("Enter Number Between (0, 35) range: ");
-            bool isBetingNumber = int.TryParse(Console.ReadLine(), out int roulletNumber);
+            bool isBetingNumber = int.TryParse(Console.ReadLine(), out int rouletteNumber);
 
             if (isBetingNumber && isBetingColor)
             {
-                if (ThirGameModeValidator(roulletColor, roulletNumber))
-                {
-                    if (ComputerRandomNumberColor == roulletNumber)
+                if ((rouletteColor == 1 || rouletteColor == 2) && (rouletteNumber >= 0 && rouletteNumber < 36))
+                {       //19 witeli
+                    if (RandomNumber == rouletteNumber && rouletteColor == 1 && !red.Contains(rouletteNumber) && !red.Contains(RandomNumber))
                     {
-                        balance += betMoney;
-                        Console.WriteLine($"you Won {5 * betMoney} current balance: {balance}");
-                        Console.WriteLine($"you color {roulletColor} number {roulletNumber} computer number {ComputerRandomNumberColor}");
+                        balance += 2 * betMoney + betMoney / 5;
+                        Console.WriteLine($"you Won {2 * betMoney + betMoney / 5} current balance: {balance}");
+                        Console.WriteLine($"you color {rouletteColor} number {rouletteNumber} computer number {RandomNumber}");
+                    }
+                    else if (RandomNumber == rouletteNumber && rouletteColor == 2 && red.Contains(rouletteNumber) && red.Contains(RandomNumber))
+                    {
+                        balance += 2 * betMoney + betMoney / 5;
+                        Console.WriteLine($"you Won {2 * betMoney + betMoney / 5} current balance: {balance}");
+                        Console.WriteLine($"you color {rouletteColor} number {rouletteNumber} computer number {RandomNumber}");
+                    }
+                    else if (RandomNumber == rouletteNumber)
+                    {
+                        if (rouletteNumber == 0)
+                        {
+                            balance += 36 * betMoney;
+                            Console.WriteLine($"its Zero you Won 36X current balance: {balance}");
+                        }
+                        balance += 2 * betMoney;
+                        Console.WriteLine($"you Won {2 * betMoney} current balance: {balance}");
+                    }
+                    else if (rouletteColor == 1 && !red.Contains(RandomNumber))
+                    {
+                        balance += betMoney / 5;
+                        Console.WriteLine($"you Won {betMoney / 5} current balance: {balance}");
+                    }
+                    else if (rouletteColor == 2 && red.Contains(RandomNumber))
+                    {
+                        balance += betMoney / 5;
+                        Console.WriteLine($"you Won {betMoney / 5} current balance: {balance}");
                     }
                     else
                     {
                         balance -= betMoney;
-                        Console.WriteLine($"You lost computer picked number: {ComputerRandomNumberRed} current balance: {balance}");
+                        Console.WriteLine($"You lost computer picked number: {RandomNumber} current balance: {balance}");
                         continue;
                     }
 
                 }
                 else
                 {
-                    Console.WriteLine("check if color matchs the number in table. or enter correct values");
+                    Console.WriteLine("enter correct collor or number");
+                    continue;
                 }
             }
             else
@@ -136,26 +164,9 @@ while (balance != 0)
                 continue;
             }
         }
-
     }
     else
     {
         continue;
     }
-}
-
-bool ThirGameModeValidator(int roulletColor, int roulletNumber)
-{
-    bool validated = false;
-
-    bool validatedColor = (roulletColor == 1 || roulletColor == 2) ? true : false;
-    bool validatedNumber = (roulletNumber >= 0 && roulletNumber < 36) ? true : false;
-
-    if (roulletColor == 1 && black.Contains(roulletNumber)) { validated = true; }
-    else if (roulletColor == 2 && red.Contains(roulletNumber)) { validated = true; }
-    else { validated = false; }
-
-
-    return validated && validatedColor && validatedNumber;
-
 }
